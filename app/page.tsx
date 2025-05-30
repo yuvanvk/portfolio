bc5a6a273458fd3bc208fe4fe655e9e3c7ab4b74
YuvanX <abhivigneshofficial@gmail.com>
2025-05-30 12:29:48 +0530

    fix: padding issue in root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between h-screen py-20">
+    <div className="flex flex-col items-center justify-between h-screen py-10 md:py-20">
       <SmoothCursor />
       <div>
         <BlurFade>
