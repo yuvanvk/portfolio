053f389fee5226106d97898e4056661f042f90fc
YuvanX <abhivigneshofficial@gmail.com>
2025-07-31 14:09:57 +0530

    fix: change herosection textsize

--- components/porfolio/hero-section.tsx
+++ components/porfolio/hero-section.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export const HeroSection = () => {
   return (
-    <div className=" font-satoshi font-light">
+    <div className=" font-satoshi font-light px-3">
       <div className="max-w-3xl flex gap-8 items-center">
         <Avatar className="w-28 h-28">
           <AvatarImage  src="/yuvanlogo.jpeg" alt="@abhi"/>
@@ -10,7 +10,7 @@ export const HeroSection = () => {
         </Avatar>
 
         <div>
-          <div className="text-4xl font-bold font-satoshi lowercase">AbhiVignesh</div>
+          <div className="text-3xl md:text-4xl font-bold font-satoshi lowercase">AbhiVignesh</div>
           <div className="text-sm">@abhiiscool</div>
 
           <div className="my-3 lowercase">
