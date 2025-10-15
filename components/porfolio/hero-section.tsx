4294d3b800c83fc08af8d5b8143a829bffee3c93
YuvanX <abhivigneshofficial@gmail.com>
2025-10-15 07:41:14 +0530

    fix: responsive

--- components/porfolio/hero-section.tsx
+++ components/porfolio/hero-section.tsx
@@ -1,7 +1,7 @@
 
 export const HeroSection = () => (
   <>
-    <p className="font-mono px-2 py-10">
+    <p className="font-mono text-sm md:text-lg px-2 py-10">
       I like clean design and building apps that feel simple and intuitive.
       Currently exploring ways to combine design and code to create better
       experiences.
