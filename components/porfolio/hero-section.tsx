42217f59df9ec44bf15136b6e3474b59d949e4fb
YuvanX <abhivigneshofficial@gmail.com>
2025-10-15 17:42:26 +0530

    fix: hero

--- components/porfolio/hero-section.tsx
+++ components/porfolio/hero-section.tsx
@@ -1,10 +1,14 @@
 
 export const HeroSection = () => (
-  <>
-    <p className="font-mono text-sm md:text-lg px-2 py-10">
+  <div className="px-2 py-10 space-y-5">
+    <p className="font-mono">
       I like clean design and building apps that feel simple and intuitive.
       Currently exploring ways to combine design and code to create better
       experiences.
     </p>
-  </>
+
+    <p className="font-mono">
+      You can find me on <a target="_blank" href="https://x.com/yuvanvk" className="underline underline-offset-4 decoration-purple-500 cursor-pointer">𝕏</a>
+    </p>
+  </div>
 );
