91fa438d88ab4821ce99425d30fb011467e5f484
yuvanvk <abhivigneshofficial@gmail.com>
2025-11-14 19:05:58 +0530

    fix: font-weight

--- components/porfolio/hero-section.tsx
+++ components/porfolio/hero-section.tsx
@@ -2,7 +2,7 @@ import { React } from "../svgs/react";
 
 
 export const HeroSection = () => (
-  <div className="px-0.5 py-10 space-y-5 font-mono font-[500] text-black/70 dark:text-white/700 tracking-tight text-[17px]">
+  <div className="px-0.5 py-10 space-y-5 font-mono font-medium text-black/70 dark:text-white/700 tracking-tight text-[16px]">
     <p>
       I&apos;m a developer who loves learning new things and building the stuff I enjoy&#46; I&apos;m passionate about creating clean&#44; functional&#44; and visually appealing experiences that solve real problems&#46;
     </p>
