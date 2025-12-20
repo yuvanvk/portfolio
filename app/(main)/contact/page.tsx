60b580f6b780b92fb6783ec06dfd91e3169fd735
yuvanvk <abhivigneshofficial@gmail.com>
2025-12-20 12:14:42 +0530

    feat: add contact and about details

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -1,5 +1,63 @@
+"use client";
+import { ContactCard } from "@/components/porfolio/contact-card";
+import { Container } from "@/components/ui/container";
+import { Asterisk } from "lucide-react";
+import { useRef } from "react";
+
 export default function ContactPage() {
-    return <div>
-        Contact page
-    </div>
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const playSound = () => {
+        if(!audioRef.current) {
+            audioRef.current = new Audio("/audio/tap_05.wav")
+        }
+
+        audioRef.current.currentTime = 0;
+        audioRef.current.play()
+    }
+    const contactInfo = [
+        {
+            id: 1,
+            type: "Email",
+            label: "abhivigneshofficial@gmail.com",
+            href: "mailto:abhivigneshofficial@gmail.com",
+            playSound,
+        },
+        {
+            id: 2,
+            type: "X",
+            label: "@yuvan",
+            href: "https://x.com/abhi_vignesh",
+            playSound,
+        },
+        {
+            id: 3,
+            type: "Instagram",
+            label: "@yuvanvk",
+            href: "https://instagram.com/abhi_vignesh",
+            playSound,
+        },
+        {
+            id: 4,
+            type: "Github",
+            label: "@yuvanvk",
+            href: "https://github.com/yuvanvk",
+            playSound,
+        }
+    ]
+    return <Container>
+        <div className="pt-20">
+            <div className="flex items-center relative gap-x-2"> 
+                <Asterisk size={30} className="absolute -top-1 -left-7 text-lime-500"/>
+                <p className="font-instrument-serif text-5xl text-center">You can contact me through</p>
+                <div className="h-px bg-neutral-700 flex-1" />
+                <div className="text-5xl font-instrument-serif">5</div>
+            </div>
+
+            <div className="py-20 flex flex-col items-center w-full gap-y-2">
+                {contactInfo.map((contact) => (
+                    <ContactCard username={contact.label} href={contact.href} key={contact.id} handleName={contact.type} playSound={contact.playSound}/>
+                ))}
+            </div>
+        </div>
+    </Container> 
 }
---
+++ components/porfolio/contact-card.tsx
@@ -0,0 +1,35 @@
+import { CircleArrowOutUpRight, Copy } from "lucide-react";
+import Link from "next/link";
+
+export const ContactCard = ({
+  username,
+  href,
+  handleName,
+  playSound
+}: {
+  username: string;
+  href: string;
+  handleName: string;
+  playSound: () => void
+}) => {
+
+
+  return (
+    <Link href={href} target="_blank" className="w-full" onMouseEnter={playSound}>
+      <div className="flex items-center gap-x-2 max-w-2xl px-2 py-2 rounded-xl hover:bg-neutral-900 tracking-tight w-full cursor-pointer">
+        <div className="group px-2 py-2 hover:bg-neutral-800  rounded-lg">
+          <Copy
+            size={15}
+            className="text-neutral-700 group-hover:text-neutral-400 transition-all "
+          />
+        </div>
+        <p>{username}</p>
+        <div className="flex-1 h-px bg-neutral-800" />
+        <p className="lowercase">{handleName}</p>
+        <div>
+          <CircleArrowOutUpRight size={15} className="text-neutral-500" />
+        </div>
+      </div>
+    </Link>
+  );
+};
