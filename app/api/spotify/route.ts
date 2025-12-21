003e19eb8cf4a2740895416a03b3735d58eec1cb
yuvanvk <abhivigneshofficial@gmail.com>
2025-12-21 12:45:20 +0530

    feat: cache last-played-song

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,9 +1,23 @@
 import { getAccessToken } from "@/lib/spotify";
 import { NextResponse } from "next/server";
+import { redis } from "@/lib/upstash-redis";
 import axios from "axios";
+
+
+const CACHE_KEY = "last-played-track"
+const CACHE_TTL = 60 * 60
 
 export async function GET() {
   try {
+
+    const cached = await redis.get(CACHE_KEY)
+
+    if(cached) {
+      return NextResponse.json({
+        track: cached
+      })
+    }
+
     const { access_token } = await getAccessToken();
 
     const res = await axios.get(
@@ -20,22 +34,28 @@ export async function GET() {
       `https://saavn.sumit.co/api/search/songs?query=${track.name}&page=0&limit=10`
     );
 
+    const finalTrack = {
+      id: track.id as string,
+      name: track.name as string,
+      artists: (track.artists as { name: string }[]).map((a) => a.name).join(", "),
+      image: track.album.images[0]?.url as string,
+      audio: audioResponseSavaan.data.data.results[0].downloadUrl[4].url as string, 
+    }
+
+    await redis.set(CACHE_KEY, finalTrack, {
+      ex: CACHE_TTL
+    })
+
     return NextResponse.json({
-      track: {
-        id: track.id as string,
-        name: track.name as string,
-        artists: (track.artists as { name: string }[])
-          .map((a) => a.name)
-          .join(", "),
-        image: track.album.images[0]?.url as string,
-        audio: audioResponseSavaan.data.data.results[0].downloadUrl[4]
-          .url as string,
-      },
+      track: finalTrack
     });
+
+
   } catch (error) {
     console.log(error);
     return NextResponse.json({
-      message: "Something went wrong",
+      message: "Failed to fetch",
+      status: 500
     });
   }
 }
